c101a2bfba507e9020e5269d15461a236bdd856c
himerumeru <himerumeru@gmail.com>
2016-03-27 18:15:23 +0900

    Refactored

--- dist/js/common.js
+++ dist/js/common.js
@@ -2,15 +2,14 @@
   chrome.runtime.onMessage.addListener(message => {
     if (message.type === 'onCompleted') {
       let currentTabId = message.tabId.toString();
-      let articles = Array.from(document.querySelectorAll('article'));
       let headings = [];
       let headingCounter = 0;
       // ストレージに保存するデータ
       // keyは eh-${tabId} とする
       let data = {};
 
-      Array.prototype.forEach.call(articles, article => {
-        Array.prototype.forEach.call(article.querySelectorAll('h1, h2, h3, h4, h5, h6'), heading => {
+      Array.from(document.querySelectorAll('article')).forEach(article => {
+        Array.from(article.querySelectorAll('h1,h2,h3,h4,h5,h6')).forEach(heading => {
           heading.insertAdjacentHTML('afterbegin', `<span id="eh-${headingCounter}"></span>`);
 
           tagName = heading.tagName.toLowerCase();
@@ -21,7 +20,7 @@
         });
       });
       data[`eh-${currentTabId}`] = headings;
-      
+
       chrome.storage.local.set(data);
       chrome.storage.local.get((data) => {
       });
